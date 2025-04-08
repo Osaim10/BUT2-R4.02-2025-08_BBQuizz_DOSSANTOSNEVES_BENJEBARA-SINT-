8f72e38850f3346bafe426c394402f83b966f875
Simao <simaoneves44@gmail.com>
2025-04-08 16:08:09 +0200

    first commit

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr8jeuQuizz/App.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr8jeuQuizz/App.java
@@ -1,10 +1,11 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr8jeuQuizz;
 
-/**
- * Hello world!
- */
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr08.jeuQuizz.entities.dto.QuestionnaireDTO;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
     }
 }
