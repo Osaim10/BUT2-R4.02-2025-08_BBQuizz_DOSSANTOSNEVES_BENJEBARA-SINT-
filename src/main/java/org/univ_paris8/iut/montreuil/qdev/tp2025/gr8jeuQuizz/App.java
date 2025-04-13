c14067bb5e0c2623f7093c3dca663de8225c51ba
Simao <simaoneves44@gmail.com>
2025-04-13 14:10:10 +0200

    peaufinements

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr8jeuQuizz/App.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr8jeuQuizz/App.java
@@ -1,7 +1,5 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr8jeuQuizz;
 
-import org.univ_paris8.iut.montreuil.qdev.tp2025.gr08.jeuQuizz.entities.dto.QuestionnaireDTO;
-
 public class App {
     public static void main( String[] args )
     {
