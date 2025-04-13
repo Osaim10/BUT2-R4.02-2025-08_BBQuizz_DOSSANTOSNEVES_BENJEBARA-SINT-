e48ee42424bba231055341f057c49129d5dc0a9c
Simao <simaoneves44@gmail.com>
2025-04-13 14:20:01 +0200

    création signature dans PartieServices

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr8jeuQuizz/services/interfaces/PartieServices.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr8jeuQuizz/services/interfaces/PartieServices.java
@@ -1,8 +1,9 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr8jeuQuizz.services.interfaces;
 
 import org.univ_paris8.iut.montreuil.qdev.tp2025.gr08.jeuQuizz.entities.dto.QuestionnaireDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr8.jeuQuizz.entities.dto.JoueurDTO;
 
 public interface PartieServices {
 
-    public boolean determiner_elements_partie(QuestionnaireDTO questionnaire, Jo)
+    public boolean determiner_elements_partie(QuestionnaireDTO questionnaire, JoueurDTO joueur);
 }
